5803e59e77125eb5e53b087980c1ec487f211748
Ruben Camargo <74126573+Lenegrad@users.noreply.github.com>
2022-03-02 18:33:17 -0700

    Creating Xcode project
    Adding all folders from the X code project to the repo.

---
+++ Shared/HealthInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthInformationApp.swift
+//  Shared
+//
+//  Created by Ruben Camargo on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
