1990409dbd31d37ce884db06e774eab5c6c39f92
Ruben Camargo <74126573+Lenegrad@users.noreply.github.com>
2022-03-29 11:18:02 -0600

    Sign In Page
    Created the sign in page which enables users to login and create accounts. The username and password are stored in a secure database

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '15.0'
+
+target 'Health (iOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Firebase'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/RemoteConfig'
+
+end
---
+++ Shared/HealthApp.swift
@@ -0,0 +1,33 @@
+//
+//  HealthInformationApp.swift
+//  Shared
+//
+//  Created by Ruben Camargo on 3/2/22.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct HealthApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            let viewModel = AppViewModel()
+            ContentView()
+                .environmentObject(viewModel)
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
